1d8dbc9cd2627fc8e4b4135c1042a22b3e369341
Kennadee <kennadee.ray@gmail.com>
2020-08-10 12:50:26 -0500

    markdown added

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,50 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  *[Installation](#installation)
+  *[Usage](#usage)
+  *[Contributing](#contributing)
+  *[License](#license)
+  *[Tests](#tests)
+  *[Questions](#questions)
+
+  ## Installation
+
+  ${data.installation}
+
+
+  ## Usage
+
+  ${data.usage}
+
+
+  ## Contributing 
+
+  ${data.contributing}
+
+
+  ## License 
+
+  ${data.license}
+
+
+  ## Tests
+
+  ${data.tests}
+
+
+  ## Questions
+
+  ${data.questions}
+
 
 `;
 }
